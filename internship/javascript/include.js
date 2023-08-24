451ee46316666a67e0c7358051d8f663c6626980
naveencho4 <choudharynaveen1970@gmail.com>
2023-08-24 15:27:59 +0530

    changed

--- internship/javascript/include.js
+++ internship/javascript/include.js
@@ -3,7 +3,12 @@ const btndisplaymodal=document.querySelector(".display-modal");
 const modal=document.querySelector(".modal");
 const blurr=document.querySelector(".try");
 const btnclosemodal=document.querySelector(".close-modal");
-
+const nextpage=function(){
+    location.replace("https://www.google.com");
+}
+const reload_page=function(){
+    location.reload();
+}
 
 const showmodla= function(){
     modal.classList.remove("hide");
