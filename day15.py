4a17ab4625c96679ac160f69e8d3daa4fa5812be
naveencho4 <choudharynaveen1970@gmail.com>
2023-10-02 16:54:22 +0530

    DAY 2

---
+++ day15.py
@@ -0,0 +1,18 @@
+import time
+mytime=time.strftime('%H:%M:%S')
+print(mytime)
+x=int(input('enter the value\n'))
+match x:
+    case 1:print("the value is one ")
+    case 2:print("the value is 2")
+    case _:print('the vlaue is default')
+
+for i in range(6,10):
+    print(i)
+
+x=int(input('enter the value\n'))
+while(x<4):
+    print(x)
+    x=x+1
+else:
+    print("i am in else statement")
--- hello.py
+++ hello.py
@@ -1,2 +1,32 @@
 dict1={"name":"naveen","age":19,"can_vote":True,"college":"ch"}
 print(dict1)
+variable_name="hey it is a game string using loop "
+for i in variable_name:
+    print(i)
+print(variable_name)
+x=str(input("enter the name\n"))
+print("the length of your given input is ",len(x))
+print("your given input in upper case is \n",x.upper())
+print("your given input in lower case is \n",x.lower())
+print("your given input in lower case is \n",x.rstrip('a'))
+print("your given input in lower case is \n",x.split('a'))
+print("your given input in lower case is \n",x.capitalize())
+print("the number mango is written",x.count("mango"))
+search=int(input("enter the number you want to find in array"))
+a =[10,12,30]
+q ="newgameislaunching"
+print(q.index("is"))
+print(q.find("ism"))
+
+print(q.isalnum())
+print(q.isalpha())
+print(q.islower())
+print(q.isupper())
+
+
+
+print(type(a))
+print("your element is at",a.find("12"))
+
+
+
