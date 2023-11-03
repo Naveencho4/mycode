2782bf76474ffc39d88d4da25ef800deaba7f135
naveencho4 <choudharynaveen1970@gmail.com>
2023-11-03 22:22:43 +0530

    learning

---
+++ startpython/forelse.py
@@ -0,0 +1,15 @@
+for i in range(1,5,+2):
+    print(i)
+
+else:
+    print(" sorry there is no i left")
+j=[1,6,44,7,2]
+print(sorted(j))
+print(j)
+a=[]
+print("enter i values to check for you ")
+x=int(input("enter the size of array / list "))
+for i in range(x):
+    y=(input(f"value of a[{i}]="))
+    a.append(y)
+print(a[i])
--- startpython/fstring.py
+++ startpython/fstring.py
@@ -1,3 +1,28 @@
 place=str(input("enter the place you live "))
 name=str(input("enter your name "))
 print(f"my name is {name} and i live in {place}")
+def square():
+    '''trying a doc file is thisS new thing it work as comment or document file'''
+print(square.__doc__)
+s={2,4,5,2}
+a=[2,4,5,2]
+b={"2","4","5","2"}
+t=(2,4,5,2)
+di={
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+x=di["year"]
+print(di)
+print(type(x))
+print(x)
+print(type(s))
+print(s)
+print(type(a))
+print(a)
+print(type(b))
+print(b)
+print(type(t))
+print(t)
